68e73c1d30469efebcca3eb869521e2cb04e5e23
Zerteg <fendrickov2017@yandex.ru>
2024-10-11 13:32:25 +0300

    server and handlers

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	todo "github.com/Zerteg/todo-app-go"
 	"github.com/Zerteg/todo-app-go/pkg/handler"
+	"github.com/Zerteg/todo-app-go/pkg/repository"
+	"github.com/Zerteg/todo-app-go/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.IntRoutes()); err != nil {
