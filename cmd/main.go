0ba4acb44f34aa8a075954d47221bcaf49a3ae80
Zerteg <fendrickov2017@yandex.ru>
2024-10-11 23:29:32 +0300

    docker bd dependence

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,27 @@ import (
 	"github.com/Zerteg/todo-app-go/pkg/handler"
 	"github.com/Zerteg/todo-app-go/pkg/repository"
 	"github.com/Zerteg/todo-app-go/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.IntRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.IntRoutes()); err != nil {
 		log.Fatalf("error occured while runnung http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.down.sql
@@ -0,0 +1,10 @@
+DROP TABLE users;
+
+DROP TABLE todo_list;
+
+DROP TABLE users_lists;
+
+DROP TABLE todo_items;
+
+DROP TABLE list_items;
+
